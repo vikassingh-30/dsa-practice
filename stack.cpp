89a8b3bbeb02355f4c3229e465e12aaf514bfeaa
vikassingh-30 <vikassingh63927@gmail.com>
2026-05-30 12:13:24 +0000

    learning stack

---
+++ stack.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Stack{
+    public:
+    int arr[5];
+    int idx=-1;
+    void push(int val){
+        idx++;
+        arr[idx]=val;
+    }
+    void pop(){
+        idx--;
+    }
+    int top(){
+        return arr[idx];
+    }
+    int size(){
+        return idx+1;
+    }
+};
+int main(){
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    cout<<st.size()<<endl;
+    st.pop();
+    cout<<st.size()<<endl;
+    cout<<st.top()<<endl;
+}
